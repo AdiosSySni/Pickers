f2bb5124bfb1fdb086c8d28c449e860f5e26da3a
withoutemot1on <pavelftemov9@gmail.com>
2022-11-14 19:48:23 +0200

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics.Metrics;
+
+namespace Picket;
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<Person> people { get; set; }
+    //public Person person { get; set; }
+    //public Person person1{ get; set; }
+
+    public MainPage()
+	{
+        people = new ObservableCollection<Person>();
+       
+
+        //person = new Person();
+        //person.Name = "Alexander";
+        //person.Surname = "Vladimir";
+        //person.Patronomic = "Sanchis";
+
+        //person1 = new Person();
+        //person1.Name = "Alexander1";
+        //person1.Surname = "Vladimi1r";
+        //person1.Patronomic = "Sanc1his";
+
+        people.Add(new Person("Paul", "Vlad", "Pavlov"));
+        people.Add(new Person("Elena", "Alexander", "Pavlov"));
+
+        BindingContext = this;
+
+        InitializeComponent();
+    }
+       
+
+	public void Picker_SelectedIndexChanged(object sender, EventArgs e)
+	{
+        
+    }
+
+}
+
---
+++ Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Picket
+{
+    public class Person
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+
+        public Person(string surname, string name, string patronymic)
+        {
+            Surname = surname;
+            Name = name;
+            Patronymic = patronymic;
+        }   
+    }
+}
